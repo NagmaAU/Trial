6b91c7944ed589edca29f6676b24855fb05be24e
NagmaAU <nagma27.786@gmail.com>
2024-06-01 22:55:11 +0530

    second trial

---
+++ JavaTraining/src/JavaBasics/DataTypes.java
@@ -0,0 +1,18 @@
+package JavaBasics;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println();
+		System.out.println();
+		
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+
+	}
+
+}
